232b38428d1f7b386a65f13180411bbd950fb79b
Vo-Tan-Sang <21211tt0917@mail.tdc.edu.vn>
2023-05-20 15:38:32 +0700

    z

--- Doan_BE2/app/Http/Controllers/ProductController.php
+++ Doan_BE2/app/Http/Controllers/ProductController.php
@@ -92,44 +92,44 @@ class ProductController extends Controller
         return view('admin.index')->with('admin.edit_product',$manager_product);
     } 
     public function update_product(Request $request,$product_id){
-        // if($request->has('product_image')){  
-        //     $file = $request->product_image;      
-        //     $new_image =$file->getClientOriginalName();
-        //     $data = array();
-        //     $data['product_name'] = $request->product_name;
-        //     $data['product_price'] = $request->product_price;  
-        //     $data['product_decs'] = $request->product_desc;
-        //     $data['product_content'] = $request->product_content;  
-        //     $data['category_id'] = $request->product_cate;      
-        //     $data['brand_id'] = $request->product_brand;  
-        //     $data['product_status'] = $request->product_status;   
-        //     $data['product_image'] = $new_image;  
-        //     $file->move(public_path('up'),$new_image);
-        //     $data['product_image'] = $new_image;         
-        //     DB::table('product')->where('product_id',$product_id)->update($data);
-        //     Session::put('message','Thêm sản phẩm thành công');
-        //     return Redirect::to('/ad/all_product');
-        //  }
-        $data = array();
-        $data['product_name'] = $request->product_name;
-        $data['product_price'] = $request->product_price;  
-        $data['product_decs'] = $request->product_desc;
-        $data['product_content'] = $request->product_content;  
-        $data['category_id'] = $request->product_cate;      
-        $data['brand_id'] = $request->product_brand;  
-        $data['product_status'] = $request->product_status;   
-        $get_image =$request->file('product_image');
-        if($get_image){        
-            $new_image = rand(0,99).'.'.$get_image->getClientOriginalExtension();
-            $get_image->move('public/up',$new_image);
+        if($request->has('product_image')){  
+            $file = $request->product_image;      
+            $new_image =$file->getClientOriginalName();
+            $data = array();
+            $data['product_name'] = $request->product_name;
+            $data['product_price'] = $request->product_price;  
+            $data['product_decs'] = $request->product_desc;
+            $data['product_content'] = $request->product_content;  
+            $data['category_id'] = $request->product_cate;      
+            $data['brand_id'] = $request->product_brand;  
+            $data['product_status'] = $request->product_status;   
+            $data['product_image'] = $new_image;  
+            $file->move(public_path('up'),$new_image);
             $data['product_image'] = $new_image;         
             DB::table('product')->where('product_id',$product_id)->update($data);
-            Session::put('message','Update sản phẩm thành công');   
+            Session::put('message','Thêm sản phẩm thành công');
             return Redirect::to('/ad/all_product');
-        }       
-        DB::table('product')->where('product_id',$product_id)->update($data);
-        Session::put('message','Update sản phẩm thành công');
-         return Redirect::to('/ad/all_product');
+         }
+        // $data = array();
+        // $data['product_name'] = $request->product_name;
+        // $data['product_price'] = $request->product_price;  
+        // $data['product_decs'] = $request->product_desc;
+        // $data['product_content'] = $request->product_content;  
+        // $data['category_id'] = $request->product_cate;      
+        // $data['brand_id'] = $request->product_brand;  
+        // $data['product_status'] = $request->product_status;   
+        // $get_image =$request->file('product_image');
+        // if($get_image){        
+        //     $new_image = rand(0,99).'.'.$get_image->getClientOriginalExtension();
+        //     $get_image->move('public/up',$new_image);
+        //     $data['product_image'] = $new_image;         
+        //     DB::table('product')->where('product_id',$product_id)->update($data);
+        //     Session::put('message','Update sản phẩm thành công');   
+        //     return Redirect::to('/ad/all_product');
+        // }       
+        // DB::table('product')->where('product_id',$product_id)->update($data);
+        // Session::put('message','Update sản phẩm thành công');
+        //  return Redirect::to('/ad/all_product');
     }
     public function delete_product($product_id){
         DB::table('product')->where('product_id',$product_id)->delete();
