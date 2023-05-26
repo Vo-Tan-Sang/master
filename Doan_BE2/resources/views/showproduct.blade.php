f055c4fc0bc62dd665990a5377dc04e8322db0a9
Nhatleo <120942667+Nhatleo@users.noreply.github.com>
2023-05-26 19:14:31 +0700

    .

--- Doan_BE2/resources/views/showproduct.blade.php
+++ Doan_BE2/resources/views/showproduct.blade.php
@@ -16,20 +16,7 @@
     <link rel="stylesheet" title="style" href="front/css/style.css">
     <link rel="stylesheet" href="front/css/animate.css">
     <link rel="stylesheet" title="style" href="front/css/huong-style.css">
-	<base href="{{asset('/')}}">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Levents - Chi tiet san pham </title>
-	<link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="front/css/font-awesome.min.css">
-	<link rel="stylesheet" href="front/vendors/colorbox/example3/colorbox.css">
-	<link rel="stylesheet" href="front/rs-plugin/css/settings.css">
-	<link rel="stylesheet" href="front/rs-plugin/css/responsive.css">
-	<link rel="stylesheet" title="style" href="front/css/style.css">
-	<link rel="stylesheet" href="front/css/animate.css">
-	<link rel="stylesheet" title="style" href="front/css/huong-style.css">
+	
 </head>
 
 <body>
@@ -133,31 +120,9 @@
                 </div>
                 <div class="clearfix"></div>
             </div> <!-- .container -->
-        </div> <!-- .header-body -->
-        <div class="header-bottom" style="background-color: #0277b8;">
-            <div class="container">
-                <a class="visible-xs beta-menu-toggle pull-right" href="#"><span
-                        class='beta-menu-toggle-text'>Menu</span> <i class="fa fa-bars"></i></a>
-                <div class="visible-xs clearfix"></div>
-                <nav class="main-menu">
-                    <ul class="l-inline ov">
-                        <li><a href="/">Trang chủ</a></li>
-                        <li><a href="#">Sản phẩm</a>
-                            <ul class="sub-menu">
-                            </ul>
-                        </li>
-                        <li><a href="about.html">Giới thiệu</a></li>
-                        <li><a href="/ad/Post_SP">Bài Viết</a></li>
-                    </ul>
-                    <div class="clearfix"></div>
-                </nav>
-            </div> <!-- .container -->
-        </div> <!-- .header-bottom -->
+        </div> <!-- .header-body -->      
     </div> <!-- #header -->
-									<div class="center">
-										<div class="space10">&nbsp;</div>
-										<a href="checkout.html" class="beta-btn primary text-center">Đặt hàng <i class="fa fa-chevron-right"></i></a>
-									</div>
+								
 								</div>
 							</div>
 						</div> <!-- .cart -->
